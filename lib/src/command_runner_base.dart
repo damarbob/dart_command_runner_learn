a9abc5a4888f7279d50dee7e7220c400a11e7a8c
Damar Syah Maulana <ibn.damr@gmail.com>
2026-05-12 16:36:34 +0700

    Implement comprehensive argument parsing and error handling
    - Add `onError` callback to `CommandRunner` for custom error processing.
    - Refactor `run` to catch exceptions and trigger `onError` if provided.
    - Expand `parse` logic to validate commands, options, and positional arguments.
    - Support both flags and options with values in the command line interface.
    - Ensure only one command and one positional argument are allowed per execution.
    - Add `_removeDashes` utility to handle shorthand and long-form option prefixes.

--- lib/src/command_runner_base.dart
+++ lib/src/command_runner_base.dart
@@ -1,24 +1,123 @@
+import 'dart:async';
 import 'dart:collection';
 import 'dart:io';
 import 'arguments.dart';
+import 'exceptions.dart';
 
 class CommandRunner {
+  FutureOr<void> Function(Object)? onError;
+
+  CommandRunner({this.onError});
+
   final Map<String, Command> _commands = {};
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView({..._commands.values});
 
   Future<void> run(List<String> args) async {
-    final ArgResults results = parse(args);
-    if (results.command != null) {
-      Object? output = await results.command!.run(results);
-      print(output.toString());
+    try {
+      final ArgResults results = parse(args);
+      if (results.command != null) {
+        Object? output = await results.command!.run(results);
+        print(output.toString());
+      }
+    } on Exception catch (exception) {
+      if (onError != null) {
+        onError!(exception);
+      } else {
+        rethrow;
+      }
     }
   }
 
   ArgResults parse(List<String> args) {
     var results = ArgResults();
-    results.command = _commands[args.first];
+    if (args.isEmpty) return results;
+
+    // Throw an exception if the command is not recognized
+    if (_commands.containsKey(args.first)) {
+      results.command = _commands[args.first];
+      args = args.sublist(1);
+    } else {
+      throw ArgumentException(
+        'The first word of input must be a command',
+        null,
+        args.first,
+      );
+    }
+
+    // Throw an exception if multiple commands are provided.
+    if (results.command != null &&
+        args.isNotEmpty &&
+        _commands.containsKey(args.first)) {
+      throw ArgumentException(
+        'Input can only contain one command. Got ${args.first} and ${results.command!.name}',
+        null,
+        args.first,
+      );
+    }
+
+    // Setion: Handle options, including flags.
+    Map<Option, Object?> argsOptionMap = {};
+    int i = 0;
+    while (i < args.length) {
+      final arg = args[i];
+      if (arg.startsWith("-")) {
+        final baseArg = _removeDashes(arg);
+        // Throw an exception if the option is not recognized
+        var option = results.command!.options.firstWhere(
+          (opt) => opt.name == baseArg || opt.abbr == baseArg,
+          orElse: () => throw ArgumentException(
+            "The option $arg is not recognized",
+            null,
+            arg,
+          ),
+        );
+
+        if (option.type == OptionType.flag) {
+          argsOptionMap[option] = true;
+          i++;
+          continue;
+        }
+
+        if (option.type == OptionType.option) {
+          // Throw an exception if the option is not followed by a value
+          if (i + 1 >= args.length) {
+            throw ArgumentException(
+              "The option ${option.name} must be followed by a value",
+              results.command!.name,
+              option.name,
+            );
+          }
+
+          if (args[i + 1].startsWith("-")) {
+            throw ArgumentException(
+              "The option ${option.name} must be followed by a value. Got another option instead '${args[i + 1]}'.",
+              results.command!.name,
+              option.name,
+            );
+          }
+
+          var arg = args[i + 1];
+          // set the value
+          argsOptionMap[option] = arg;
+          i++;
+        }
+      } else {
+        // Throw an exception if more than one positional argument is provided
+        if (results.commandArg != null && results.commandArg!.isNotEmpty) {
+          throw ArgumentException(
+            'Commands can only have up to one argument.',
+            results.command!.name,
+            arg,
+          );
+        }
+        results.commandArg = arg;
+      }
+      i++;
+    }
+    results.options = argsOptionMap;
+
     return results;
   }
 
@@ -33,5 +132,15 @@ class CommandRunner {
   String get usage {
     final exeFile = Platform.script.path.split('/').last;
     return "Usage: dart bin/$exeFile [command] [arguments?] [...options?]";
+  }
+
+  String _removeDashes(String arg) {
+    if (arg.startsWith("--")) {
+      return arg.substring(2);
+    }
+    if (arg.startsWith("-")) {
+      return arg.substring(1);
+    }
+    return arg;
   }
 }
